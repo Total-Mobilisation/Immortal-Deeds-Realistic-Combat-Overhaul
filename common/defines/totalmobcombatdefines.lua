e5189f7f9fa1850a7a5669e3576e74c962bce4ee
Bryce <77906727+nice-bryce@users.noreply.github.com>
2025-09-21 09:45:16 +1000

    Veterancy nerfed and brought back into 'low level = lower attack'
    Lowest troop is 50%

--- common/defines/totalmobcombatdefines.lua
+++ common/defines/totalmobcombatdefines.lua
@@ -4,3 +4,7 @@ NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.0277 -- Org is now much h
 
 NDefines.NMilitary.LAND_AIR_COMBAT_STR_DAMAGE_MODIFIER	 = 0.0095 -- vanilla is 0.04. In vanilla, the main benefit of Air is that it bypass defense.
 NDefines.NMilitary.LAND_AIR_COMBAT_ORG_DAMAGE_MODIFIER	 = 0.0285 -- 0.00245 Org damage is 6X now.
+
+-- Adjusts experience factor
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.1
+NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 4
