1395a8dc392f246fa506a68fcd8bc7ca90f2ebfd
Bryce <77906727+nice-bryce@users.noreply.github.com>
2025-09-20 11:40:59 +1000

    Initial commit

---
+++ common/defines/totalmobcombatdefines.lua
@@ -0,0 +1,6 @@
+-- Adjusts Org/Strength Damage
+NDefines.NMilitary.LAND_COMBAT_STR_DAMAGE_MODIFIER = 0.02 -- 
+NDefines.NMilitary.LAND_COMBAT_ORG_DAMAGE_MODIFIER = 0.0277 -- Org is now much higher than before. Division will retreat when 1/5.7th are casualties (approx 30% of battalion).
+
+NDefines.NMilitary.LAND_AIR_COMBAT_STR_DAMAGE_MODIFIER	 = 0.0095 -- vanilla is 0.04. In vanilla, the main benefit of Air is that it bypass defense.
+NDefines.NMilitary.LAND_AIR_COMBAT_ORG_DAMAGE_MODIFIER	 = 0.0285 -- 0.00245 Org damage is 6X now.
---
+++ descriptor.mod
@@ -0,0 +1,9 @@
+version="0.3.0"
+tags={
+	"Gameplay"
+	"Balance"
+	"Historical"
+	"Military"
+}
+name="Immortal Deeds - Realistic Combat Overhaul"
+supported_version="1.16.9"
