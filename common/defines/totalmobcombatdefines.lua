c46789b6be5133de5b4a7aab0d6b51c9fdb55b8e
Bryce <77906727+nice-bryce@users.noreply.github.com>
2025-09-29 20:43:48 +1000

    define changes

--- common/defines/totalmobcombatdefines.lua
+++ common/defines/totalmobcombatdefines.lua
@@ -8,8 +8,34 @@ NDefines.NMilitary.LAND_AIR_COMBAT_ORG_DAMAGE_MODIFIER	 = 0.0285 -- 0.00245 Org 
 -- Adjusts experience factor
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.1
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 4
+
+-- Reinforcing is easier
 NDefines.NMilitary.REINFORCE_CHANCE = 0.12
 NDefines.NMilitary.SPEED_REINFORCEMENT_BONUS = 0.02
-NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.15
-NDefines.NMilitary.RIVER_CROSSING_PENALTY_LARGE = -0.3
+
+-- Adjusts terrain/fort penalty
+NDefines.NMilitary.RIVER_CROSSING_PENALTY = -0.05
+NDefines.NMilitary.RIVER_CROSSING_PENALTY_LARGE = -0.1
 NDefines.NMilitary.BASE_FORT_PENALTY = -0.1
+
+-- Attaches are cheaper and nerfer
+NDefines.NDiplomacy.BASE_SEND_ATTACHE_COST = 10
+NDefines.NDiplomacy.BASE_SEND_ATTACHE_CP_COST = 5
+NDefines.NCountry.ATTACHE_XP_SHARE = 0
+
+-- Upkeep attrition
+NDefines.NMilitary.UNIT_UPKEEP_ATTRITION = 0.0001
+
+-- Adjusts Equipment Losses
+NDefines.NMilitary.EQUIPMENT_COMBAT_LOSS_FACTOR = 1
+NDefines.NMilitary.RELIABILTY_RECOVERY = 0
+NDefines.NMilitary.BASE_CAPTURE_EQUIPMENT_RATIO = 0.14
+
+-- The Bomber Always Gets Through
+NDefines.NAir.DISRUPTION_FACTOR = 0 -- 4
+NDefines.NAir.DISRUPTION_FACTOR_CARRIER = 0.8 -- 8
+NDefines.NAir.ESCORT_FACTOR = 2 -- 2
+
+-- Lower army cap
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP  = 12
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 12
